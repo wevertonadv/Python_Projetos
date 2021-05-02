f79443221d10f87c993844e2950541b37575d76d
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2021-05-02 12:58:53 -0300

    Importando o programa 11
    Desenvolvi o programa 11 que tem como objetivo sortear 5 nomes aleatorios.

--- Programas em Python/100-Projetos.py
+++ Programas em Python/100-Projetos.py
@@ -120,3 +120,16 @@ if computador == jogador:
     print('Parabéns Voce ganhou com o número {}'.format(jogador))
 else:
     print('Você perdeu o número pensado foi {}'.format(computador))
+
+#SORTEANDO 5 NOMES ALEATÓRIOS
+#DESAFIO 11
+from random import choice
+print('|Sorteando  Nomes|')
+nome1 = input("Participante 01: ")
+nome2 = input("Participante 02: ")
+nome3 = input("Participante 03: ")
+nome4 = input("Participante 04: ")
+nome5 = input("Participante 05: ")
+participantes = [nome1, nome2, nome3, nome4, nome5]
+escolhodo = choice(participantes)
+print("Parabéns {} você ganhou".format(escolhodo))
