b428d1a0020fffae198730ce191c752687c585a7
weverton de mello <62575377+wevertonadv@users.noreply.github.com>
2022-06-15 20:15:19 -0300

    Aluguel de Carros

--- Programas em Python/19-Programas/__init__.py
+++ Programas em Python/19-Programas/__init__.py
@@ -1,4 +1,5 @@
-# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu
+# novo salário, com 15% de aumento.
 
 salario = float(input("Digite o salario: "))
 aumento = salario + (salario * 15 / 100)
